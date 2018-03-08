23fb9b3275070512661af240e2bb00796ff3b4fb
Andy <andyyswoo@gmail.com>
2018-03-08 02:21:52 -0800

    use gated dot attention, add self-attention

---
+++ code/rnet_model.py
@@ -0,0 +1,91 @@
+from qa_model import QAModel
+from modules import RNNEncoder, masked_softmax, BasicAttn, SimpleSoftmaxLayer
+import tensorflow as tf
+from tensorflow.python.ops import variable_scope as vs
+
+class RNetModel(QAModel):
+    def build_graph(self):
+        # Word embedding only TODO char embedding, go deeper
+        with tf.variable_scope('encoding'):
+            encoder = RNNEncoder(self.FLAGS.hidden_size, self.keep_prob)
+            context_hiddens = encoder.build_graph(self.context_embs, self.context_mask) # (batch_size, context_len, hidden_size*2)
+            question_hiddens = encoder.build_graph(self.qn_embs, self.qn_mask) # (batch_size, question_len, hidden_size*2)
+
+        # Gated C2Q attention
+        with tf.variable_scope('C2Q'):
+            c2q_attn_layer = GatedDotAttn(self.keep_prob, self.FLAGS.hidden_size*2, self.FLAGS.hidden_size*2)
+            _, c2q_attention = c2q_attn_layer.build_graph(question_hiddens, self.qn_mask, context_hiddens) # (batch_size, context_len, hidden_size*4)
+            encoder = RNNEncoder(self.FLAGS.hidden_size, self.keep_prob)
+            c2q_attention = encoder.build_graph(c2q_attention, self.context_mask) # (batch_size, context_len, hidden_size*2)
+
+        with tf.variable_scope('selfmatching'):
+            self_attn_layer = GatedDotAttn(self.keep_prob, self.FLAGS.hidden_size*2, self.FLAGS.hidden_size*2)
+            _, self_attention = self_attn_layer.build_graph(c2q_attention, self.context_mask, c2q_attention) # (batch_size, context_len, hidden_size*4)
+            encoder = RNNEncoder(self.FLAGS.hidden_size, self.keep_prob)
+            self_attention = encoder.build_graph(self_attention, self.context_mask) # (batch_size, context_len, hidden_size*2)
+
+        # Apply fully connected layer to each blended representation
+        # Note, blended_reps_final corresponds to b' in the handout
+        # Note, tf.contrib.layers.fully_connected applies a ReLU non-linarity here by default
+        blended_reps_final = tf.contrib.layers.fully_connected(self_attention, num_outputs=self.FLAGS.hidden_size) # blended_reps_final is shape (batch_size, context_len, hidden_size)
+
+        # Use softmax layer to compute probability distribution for start location
+        # Note this produces self.logits_start and self.probdist_start, both of which have shape (batch_size, context_len)
+        with vs.variable_scope("StartDist"):
+            softmax_layer_start = SimpleSoftmaxLayer()
+            self.logits_start, self.probdist_start = softmax_layer_start.build_graph(blended_reps_final, self.context_mask)
+
+        # Use softmax layer to compute probability distribution for end location
+        # Note this produces self.logits_end and self.probdist_end, both of which have shape (batch_size, context_len)
+        with vs.variable_scope("EndDist"):
+            softmax_layer_end = SimpleSoftmaxLayer()
+            self.logits_end, self.probdist_end = softmax_layer_end.build_graph(blended_reps_final, self.context_mask)
+
+class GatedDotAttn(BasicAttn):
+    def build_graph(self, values, values_mask, keys):
+        """
+        Keys attend to values.
+        For each key, return an attention distribution and an attention output vector.
+
+        Inputs:
+          values: Tensor shape (batch_size, num_values, value_vec_size).
+          values_mask: Tensor shape (batch_size, num_values).
+            1s where there's real input, 0s where there's padding
+          keys: Tensor shape (batch_size, num_keys, value_vec_size)
+
+        Outputs:
+          attn_dist: Tensor shape (batch_size, num_keys, num_values).
+            For each key, the distribution should sum to 1,
+            and should be 0 in the value locations that correspond to padding.
+          output: Tensor shape (batch_size, num_keys, hidden_size).
+            This is the attention output; the weighted sum of the values
+            (using the attention distribution as weights).
+        """
+        with vs.variable_scope("GatedDotAttn"):
+            # Calculate attention distribution
+            values_t = tf.transpose(values, perm=[0, 2, 1]) # (batch_size, value_vec_size, num_values)
+            attn_logits = tf.matmul(keys, values_t) # shape (batch_size, num_keys, num_values)
+            attn_logits_mask = tf.expand_dims(values_mask, 1) # shape (batch_size, 1, num_values)
+            _, attn_dist = masked_softmax(attn_logits, attn_logits_mask, 2) # shape (batch_size, num_keys, num_values). take softmax over values
+
+            # Use attention distribution to take weighted sum of values
+            output = tf.matmul(attn_dist, values) # shape (batch_size, num_keys, value_vec_size)
+
+            # Apply dropout
+            output = tf.nn.dropout(output, self.keep_prob)
+
+            # Blend
+            output = tf.concat([keys, output], axis=2)
+
+        # Compute gate
+        with tf.variable_scope('c2qgate'):
+            shape = tf.shape(output)
+            dim = output.get_shape().as_list()[-1]
+            flatten = tf.reshape(output, (-1, dim))
+            W = tf.get_variable('Wc2gate', (dim, dim))
+            gate = tf.matmul(flatten, W)
+            gate = tf.reshape(gate, shape)
+            gate = tf.nn.sigmoid(gate)
+            output = gate * output
+            return attn_dist, output
+
